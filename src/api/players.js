a4f9e50630f73633be7376a1478680ebef472595
Garnet Ni <garnet2333@hotmail.com>
2023-03-13 22:08:37 +0000

    added slight responsiveness

--- src/api/players.js
+++ src/api/players.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 export const getPlayers = async (query) => {
-    try {
-        const res = await axios('https://gamification-trading-default-rtdb.firebaseio.com/players.json')
-        return res.data
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    const res = await axios(process.env.VITE_FIREBASE_URL);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
--- src/components/Leaderboard/BoardContentCard/BoardContent/BoardContent.jsx
+++ src/components/Leaderboard/BoardContentCard/BoardContent/BoardContent.jsx
@@ -1,26 +1,27 @@
-import BoardFilter from './BoardFilter'
-import BoardList from './BoardList'
-import BoardPages from './BoardPages'
-import PlayerModal from './PlayerModal'
-import { useEffect, useContext } from 'react'
-import { useLocation } from 'react-router-dom'
-import LeaderboardContext from '../../../../store/leaderboard-context'
-import { AnimatePresence, motion } from 'framer-motion'
+import BoardFilter from "./BoardFilter";
+import BoardList from "./BoardList";
+import BoardPages from "./BoardPages";
+import PlayerModal from "./PlayerModal";
+import { useEffect, useContext } from "react";
+import { useLocation } from "react-router-dom";
+import LeaderboardContext from "../../../../store/leaderboard-context";
 
 const BoardContent = () => {
-    const { onChangePath } = useContext(LeaderboardContext)
-    const { pathname } = useLocation()
+  const { onChangePath } = useContext(LeaderboardContext);
+  const { pathname } = useLocation();
 
-    useEffect(() => {
-        onChangePath(pathname)
-    }, [pathname, onChangePath])
+  useEffect(() => {
+    onChangePath(pathname);
+  }, [pathname, onChangePath]);
 
-    return <div className='mx-0'>
-        <BoardFilter />
-        <BoardList />
-        <BoardPages />
-        <PlayerModal />
+  return (
+    <div className="mx-0">
+      <BoardFilter />
+      <BoardList />
+      <BoardPages />
+      <PlayerModal />
     </div>
-}
+  );
+};
 
-export default BoardContent
+export default BoardContent;
